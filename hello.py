e2117e3c95406bec05dd0b62ccb3cd06155122a0
Vaibhav-Adv <jeevanconnect25@gmail.com>
2025-06-03 08:02:21 +0000

    Initial commit

--- hello.py
+++ hello.py
@@ -6,8 +6,8 @@ app = FastAPI()
 
 wa = WhatsApp(
     phone_id='715412771651848',
-    token='EAAQL8ZB2cPX0BO8i2jr8qm1Np5rEaf0K9k4CZBdXsnZCC0ZAWyQgDiKb8JSrMlGYtcXS4eOSsBvLt35i7qRS9RkmS4WheYWtbkgChHB3VTs5ZCq1BgLPY3ZAvICiMKQrHEpwDYaAgXO5ZBxUrfYD9JC1N96aRhgHr7i7wKHx0Y89gW6l9fKmX20KezTWdT9iYhVHv6ueZAloJBrYTLoPqIHhcyfgeKkNHO0sPm0ZD',
-    verify_token='xyzxyz', 
+    token='EAAQL8ZB2cPX0BO7w14HeDlcKfIpdOy4GaZCrkjkdZCf21xnADcW7eh1ZAAcfvFRCRWqCYmgvX8gsZAKDIaVR7gQZCZAGapU9hqeZCmohgZCy690btTJoZCK4mhZAkMwQZCWqrV8k58fclq5xvn1KYihRp7WmggEKGX8ubAJUiZAThe7PE0XTyHb2lyy9ZCRYwHKnMLmBVHgTiRVf8l0wdlU1sfTrhUWaXTrBw5jQ7MqU0ZD',
+    verify_token='12345', 
     server=app, # Pass FastAPI app here
 )
 
